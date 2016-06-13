4e63f3d0be48d9fe7147dc94ee71a16b393d0b09
AaronJan <aaronjan@qq.com>
2016-06-13 20:20:51 +0800

    update config file template

--- src/config/housekeeper.php
+++ src/config/housekeeper.php
@@ -4,7 +4,7 @@
  *
  * @author         AaronJan <https://github.com/AaronJan/Housekeeper>
  * @package        Housekeeper
- * @version        2.0-dev
+ * @version        2.*
  * @license        Apache 2.0
  * @copyright  (c) 2015, AaronJan
  * @link           https://github.com/AaronJan/Housekeeper
